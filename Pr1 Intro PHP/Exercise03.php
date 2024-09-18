3e440c8163336725eab00cf4a7d0bb46256f41a6
woocky17 <davidortetsabater@gmail.com>
2024-09-18 18:47:17 +0200

    Segundo commit

---
+++ Pr1 Intro PHP/Exercise03.php
@@ -0,0 +1,18 @@
+<?php
+/*
+1. Se declaren dos variables con valores numéricos superiores a 10 y seguidamente:
+*/
+
+/*
+2. Muestra la progresión numérica de los números pares desde 0 hasta el valor de la
+primera con un bucle for.
+*/
+/*
+3. Muestra la progresión numérica desde la segunda variable hasta 0 con un bucle
+while.
+/*
+4. Muestra la progresión numérica desde la primera variable a la segunda con un bucle
+do/while.
+a. Si la segunda variable es más pequeña, sólo queremos que imprima una vez
+el valor de la primera variable. 
+*/
